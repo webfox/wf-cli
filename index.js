25ba0ac9ac0121d17d6289e9aa701247169cca63
Otis Wright <otis@shipwrecked.co>
2016-09-08 20:37:49 +1200

    Added provisioner to index

--- index.js
+++ index.js
@@ -6,35 +6,38 @@ const figlet = require('figlet');
 const inquirer = require('inquirer');
 
 const generator = require('./lib/generator');
+const provisioner = require('./lib/provisioner');
 
 clear();
 console.log(
-  chalk.yellow(
-    figlet.textSync('Webfox', {horizontalLayout: 'full'})
-  )
+    chalk.yellow(
+        figlet.textSync('Webfox', {horizontalLayout: 'full'})
+    )
 );
 
 inquirer.prompt(
-  [
-    {
-      type: 'list',
-      name: 'actions',
-      message: 'What do you want to do?',
-      choices: [
-        'Generate a Site',
-        'Provision a Site',
-        'Publish a Site',
-        'Update a Site'
-      ]
-    }
-  ]
+    [
+        {
+            type: 'list',
+            name: 'actions',
+            message: 'What do you want to do?',
+            choices: [
+                'Generate a Site',
+                'Provision a Site',
+                'Publish a Site',
+                'Update a Site'
+            ]
+        }
+    ]
 ).then(function (answers) {
     switch (answers.actions) {
-      case "Generate a Site":
-        generator();
-        break;
-      default:
-        console.log('wrong one');
+        case 'Generate a Site':
+            generator();
+            break;
+        case 'Provision a Site':
+            provisioner();
+            break;
+        default:
+            console.log('This feature has not yet been implemented');
     }
-  }
-);
+});
