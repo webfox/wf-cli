a3bd93383910892784e66fc602f09444200ca7d3
Otis Wright <otis@webfox.co.nz>
2016-09-08 15:34:29 +1200

    Clones down template

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 #!/usr/bin/env node
 
-const fs          = require('fs');
-const chalk       = require('chalk');
-const clear       = require('clear');
-const figlet      = require('figlet');
-const inquirer    = require('inquirer');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
 
-const files = require('./lib/files');
+const generator = require('./lib/generator');
 
 clear();
 console.log(
   chalk.yellow(
-    figlet.textSync('Webfox', { horizontalLayout: 'full' })
+    figlet.textSync('Webfox', {horizontalLayout: 'full'})
   )
 );
 
-    inquirer.prompt(
-      [
-	{
-    type: 'list',
-    name: 'actions',
-    message: 'What do you want to do?',
-    choices: [
-      'Generate a Site',
-      'Provision a Site',
-      'Publish a Site',
-      'Update a Site'
-    ]
-  }    
+inquirer.prompt(
+  [
+    {
+      type: 'list',
+      name: 'actions',
+      message: 'What do you want to do?',
+      choices: [
+        'Generate a Site',
+        'Provision a Site',
+        'Publish a Site',
+        'Update a Site'
       ]
-    ).then(function( answers ) {
-console.log(answers);
-      }
-    );
+    }
+  ]
+).then(function (answers) {
+    switch (answers.actions) {
+      case "Generate a Site":
+        generator();
+        break;
+      default:
+        console.log('wrong one');
+    }
+  }
+);
